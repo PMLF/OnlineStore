52e05fe5d1a4d8f7eb75f014fd920679682fc9ef
Fonseca <pmlf@git.com>
2020-06-25 19:46:00 +0100

    Keyboard theme init. ADD: Contact page.

--- js/hideHeader.js
+++ js/hideHeader.js
@@ -1,11 +1,24 @@
+
+/**
+ * Checks whether to hide the header when the user scrolls or not.
+ */
+window.onscroll = function() {
+    hideHeader();
+};
+
+/**
+ * Checks whether to hide the header when the file is ready or not.
+ */
+$(document).ready(hideHeader());
+
 /**
  * Keeps track of the user's scrolling activity to hide or show header.
  */
-window.onscroll = function() {
+function hideHeader() {
     if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         document.querySelector("header").classList.add("hide");
     }
     else {
         document.querySelector("header").classList.remove("hide");
     }
-};
+}
